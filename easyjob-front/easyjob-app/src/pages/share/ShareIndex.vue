54eb296d2a677c50efdf5e083024bba6b78cd817
yin02 <1354525000@qq.com>
2024-01-08 19:14:00 -0800

    1

---
+++ easyjob-front/easyjob-app/src/pages/share/ShareIndex.vue
@@ -0,0 +1,65 @@
+<template>
+  <view>
+    <Navbar :showLeft="false" title="经验分享"></Navbar>
+    <view class="share-list">
+      <DataList
+        :dataSource="dataSource"
+        @loadData="loadDataList"
+        :loadStatus="loadStatus"
+      >
+        <template #default="{ data }">
+          <ShareItem :data="data" :showDetail="showDetail"></ShareItem>
+        </template>
+      </DataList>
+    </view>
+  </view>
+</template>
+
+<script setup>
+import ShareItem from "./ShareItem.vue";
+import { onLoad, onReachBottom } from "@dcloudio/uni-app";
+import { ref, reactive, getCurrentInstance, nextTick } from "vue";
+const { proxy } = getCurrentInstance();
+
+const props = defineProps({
+  showDetail: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const dataSource = ref({});
+const loadStatus = ref(null);
+const loadDataList = async () => {
+  loadStatus.value = "loading";
+  let result = await proxy.Request({
+    url: proxy.Api.loadShareInfo,
+    params: {
+      pageNo: dataSource.value.pageNo,
+    },
+  });
+  if (!result) {
+    return;
+  }
+  loadStatus.value = "more";
+  dataSource.value = result.data;
+};
+
+onLoad(() => {
+  loadDataList();
+});
+
+onReachBottom(() => {});
+</script>
+
+
+<style>
+page {
+  background: #f3f3f3;
+}
+</style>
+<style lang="scss" scoped>
+.share-list {
+  padding: 5px;
+}
+</style>
